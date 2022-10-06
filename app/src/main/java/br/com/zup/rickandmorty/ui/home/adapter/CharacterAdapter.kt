00c2ff5a3e00e54e465940efc3344d7f97291b09
Luiz Filipe Medeira <luiz.fmdr@gmail.com>
2022-10-06 09:01:32 -0300

    ui improvement

--- app/src/main/java/br/com/zup/rickandmorty/ui/home/adapter/CharacterAdapter.kt
+++ app/src/main/java/br/com/zup/rickandmorty/ui/home/adapter/CharacterAdapter.kt
@@ -3,22 +3,24 @@ package br.com.zup.rickandmorty.ui.home.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.zup.rickandmorty.core.loadImage
 import br.com.zup.rickandmorty.data.model.CharacterResult
 import br.com.zup.rickandmorty.databinding.CharacterItemBinding
 import com.squareup.picasso.Picasso
 
 class CharacterAdapter(
     private var characterList: MutableList<CharacterResult>,
-    private val characterClick: (characterResult : CharacterResult) -> Unit
+    private val characterClick: (characterResult: CharacterResult) -> Unit
 ) : RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        val binding = CharacterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            CharacterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CharacterViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        val character : CharacterResult = characterList[position]
+        val character: CharacterResult = characterList[position]
         holder.showCharacterInfo(character)
         holder.binding.cvItemLista.setOnClickListener {
             characterClick(character)
@@ -32,10 +34,10 @@ class CharacterAdapter(
         notifyDataSetChanged()
     }
 
-    class CharacterViewHolder(val binding: CharacterItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class CharacterViewHolder(val binding: CharacterItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun showCharacterInfo(characterResult: CharacterResult) {
-            Picasso.get().load(characterResult.image)
-                .into(binding.ivCharacter)
+            binding.ivCharacter.loadImage(characterResult.image)
             binding.tvCharacterName.text = characterResult.name
         }
     }
